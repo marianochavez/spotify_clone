5679082ae9632725f46ed1477f32eae984eaac02
marianochavez <amarianochavez@gmail.com>
2023-06-10 20:00:38 -0300

    liked songs

--- providers/UserProvider.tsx
+++ providers/UserProvider.tsx
@@ -6,7 +6,7 @@ type UserProviderProps = {
   children: React.ReactNode;
 };
 
-const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+const UserProvider = ({ children }: UserProviderProps) => {
   return <MyUserContextProvider>{children}</MyUserContextProvider>;
 };
 
